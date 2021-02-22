924b14490b4c979298a86710e905d40c234da796
dfox <1253980580@qq.com>
2021-02-22 16:19:25 +0800

    增加方法

--- src/index.js
+++ src/index.js
@@ -21,3 +21,61 @@ export const loadCssJs = (url) => {
   }
   document.body.appendChild(dom)
 }
+
+/**
+ * 获取地址栏get参数
+ * @param {String} str key值
+ */
+export const getUrlParams = (str) => {
+  var reg = new RegExp('(^|&)' + str + '=([^&]*)(&|$)')
+  var r = window.location.search.substr(1).match(reg)
+  if (r !== null) {
+    return unescape(r[2])
+  } else {
+    return null
+  }
+}
+
+/**
+ * base64图片转bytes
+ * @param {String} url base64图片
+ */
+export const baseImgToBytes = (url) => {
+  var ia = []
+  var data = url.split(',')[1]
+  data = window.atob(data)
+  for (var i = 0; i < data.length; i++) {
+    ia[i] = data.charCodeAt(i)
+  }
+  return ia
+}
+
+/**
+ * 数组合并去重
+ */
+export const uniqueArr = () => {
+  if (arguments.length === 0) return []
+  var arr = []
+  for (let i = 0; i < arguments.length; i++) {
+    arr.push(...arguments[i])
+  }
+  return [...new Set(arr)]
+}
+
+/**
+ * 深拷贝
+ * @param {Array|Object} obj 被深拷贝的
+ */
+export const deepCopy = (obj) => {
+  var back = obj instanceof Array ? [] : {}
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        back[key] = deepCopy(obj[key])
+      } else {
+        back[key] = obj[key]
+      }
+    }
+  }
+  return back
+}
